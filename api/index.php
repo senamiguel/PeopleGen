e9cd11fb896dd4545d6c31fc797d563e5aeed9f1
Miguel Sena <miguelaugustosena@gmail.com>
2024-02-29 10:14:32 -0300

    atualizado action

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@ include "gerador.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <form action="random.php" method="get">
+    <form action="index.php" method="get">
         <label for="number" >Quantas pessoas quer gerar?</label>
         <input type="number" name="number" min="1"max="20">
         <button type="submit" class="btn btn-primary">Submit</button>
